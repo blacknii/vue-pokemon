d0d207fa657e2bf58507c2450756599609aac3ce
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-18 19:02:55 +0200

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <main>
     <h1>{{ grating }} - {{ number }}</h1>
     <p v-for="character in characters" :key="character.name">{{ character.name }}</p>
+
+    <div class="q-pa-md q-gutter-sm">
+      <q-avatar color="red" text-color="white" icon="directions" />
+      <q-avatar color="primary" text-color="white">J</q-avatar>
+      <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
+      <q-avatar size="24px" color="orange">J</q-avatar>
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/img/avatar.png" />
+      </q-avatar>
+    </div>
   </main>
 </template>
 
