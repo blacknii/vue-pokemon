f6b6bc7ddb1bd48f6927f84c7651c90d5e08d025
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-26 12:08:30 +0200

    .

--- src/views/SinglePokemonView.vue
+++ src/views/SinglePokemonView.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="single-pokemon-container" v-if="pokemon">
+    <div class="details">
+      <p><strong>id</strong>: {{ pokemon.id }}</p>
+      <p><strong>name</strong>: {{ pokemon.name }}</p>
+      <p><strong>height</strong>: {{ pokemon.height }}</p>
+      <p><strong>weight</strong>: {{ pokemon.weight }}</p>
+      <p>
+        <strong>types</strong>
+        <q-chip v-for="type in pokemon.types">
+          {{ type }}
+        </q-chip>
+      </p>
+      <p><strong>sprite</strong>: <a :href="pokemon.sprite">PokeAPI</a></p>
+      <RouterLink to="/">
+        <q-btn push color="primary" label="BACK" />
+      </RouterLink>
+    </div>
     <img :src="pokemon.sprite" :alt="pokemon.name" />
-    <p><strong>id</strong>: {{ pokemon.id }}</p>
-    <p><strong>name</strong>: {{ pokemon.name }}</p>
-    <p><strong>height</strong>: {{ pokemon.height }}</p>
-    <p><strong>weight</strong>: {{ pokemon.weight }}</p>
-    <p>
-      <strong>types</strong>
-      <q-chip v-for="type in pokemon.types">
-        {{ type }}
-      </q-chip>
-    </p>
-    <p><strong>sprite</strong> {{ pokemon.sprite }}</p>
-    <RouterLink to="/">
-      <q-btn push color="primary" label="BACK" />
-    </RouterLink>
   </div>
 </template>
 
@@ -48,9 +50,19 @@ export default defineComponent({
 
 <style scoped>
 .single-pokemon-container {
-  background-color: rgba(107, 64, 64, 0.342);
+  /* background-color: rgba(107, 64, 64, 0.342); */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
   margin: 0 auto;
   max-width: 70rem;
   padding: 1rem;
+}
+
+.details {
+  /* background-color: aqua; */
+  margin: 5rem 0 0 2rem;
+  width: 15rem;
 }
 </style>
