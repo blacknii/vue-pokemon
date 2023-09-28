5098c8d041ec01116f1b874dc7f6ccb08e0169ff
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-28 20:18:11 +0200

    .

--- src/composables/likes/addLikedPokemon.ts
+++ src/composables/likes/addLikedPokemon.ts
@@ -1,5 +1,12 @@
 const addLikedPokemon = (id: string) => {
-  console.log(`Pokemon ${id} added`)
+  const LikedPokemonArray = localStorage.getItem('LikedPokemon')
+  if (LikedPokemonArray === null) {
+    localStorage.setItem('LikedPokemon', JSON.stringify([id]))
+  } else if (LikedPokemonArray !== null) {
+    localStorage.setItem('LikedPokemon', JSON.stringify([...JSON.parse(LikedPokemonArray), id]))
+  }
+
+  console.log(`Pokemon ${id} added to liked`, LikedPokemonArray)
 }
 
 export default addLikedPokemon
--- src/composables/likes/removeLikedPokemon.ts
+++ src/composables/likes/removeLikedPokemon.ts
@@ -1,5 +1,15 @@
 const removeLikedPokemon = (id: string) => {
-  console.log(`Pokemon ${id} removed`)
+  const LikedPokemonArray = localStorage.getItem('LikedPokemon')
+  if (LikedPokemonArray === null) {
+    console.error('I am trying to remove no existing pokemon from cough')
+  } else if (LikedPokemonArray !== null) {
+    localStorage.setItem(
+      'LikedPokemon',
+      JSON.stringify(JSON.parse(LikedPokemonArray).filter((pokemonId: string) => pokemonId !== id))
+    )
+  }
+
+  console.log(`Pokemon ${id} removed from liked`, LikedPokemonArray)
 }
 
 export default removeLikedPokemon
