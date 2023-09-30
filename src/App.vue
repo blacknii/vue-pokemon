4939d99cec4caa2f37d0fdbdb38b24aefe3e6f07
blacknii <42908977+blacknii@users.noreply.github.com>
2023-09-30 21:12:42 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div class="container">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/caught">Caught</router-link>
-      <router-link to="/liked">Liked</router-link>
-      <router-link :to="'/pokemon/' + random">Random Pokemon</router-link>
-    </nav>
-    <RouterView />
+  <div>
+    <q-layout
+      view="lhh LpR lff"
+      container
+      style="height: 100vh"
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+      <q-header reveal :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'">
+        <q-toolbar>
+          <q-toolbar-title>Header</q-toolbar-title>
+          <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/caught">Caught</router-link>
+            <router-link to="/liked">Liked</router-link>
+            <router-link :to="'/pokemon/' + random">Random Pokemon</router-link>
+          </nav>
+        </q-toolbar>
+      </q-header>
+
+      <q-footer>
+        <q-toolbar>
+          <q-toolbar-title>Footer</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+
+      <q-page-container>
+        <div class="container">
+          <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/caught">Caught</router-link>
+            <router-link to="/liked">Liked</router-link>
+            <router-link :to="'/pokemon/' + random">Random Pokemon</router-link>
+          </nav>
+          <RouterView />
+        </div>
+      </q-page-container>
+    </q-layout>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RouterView } from 'vue-router'
-import { useCounterStore } from './stores/counter'
-
-const counterStore = useCounterStore()
-
-const addPokemonLike = () => {
-  counterStore.addLikedPokemonTwo(3)
-}
-const addPokemonCath = () => {
-  counterStore.addCaughtPokemonTwo(20)
-}
 
 const random = ref(Math.floor(Math.random() * 800) + 1)
 </script>
@@ -39,7 +58,10 @@ const random = ref(Math.floor(Math.random() * 800) + 1)
 nav {
   display: flex;
   gap: 1rem;
-  font-size: 2rem;
+  font-size: 1rem;
   padding: 1rem;
+}
+a {
+  color: white;
 }
 </style>
